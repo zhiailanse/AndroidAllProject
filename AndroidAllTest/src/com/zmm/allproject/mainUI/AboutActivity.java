9e96f5b75442cc2b2c839631fc6cb2bd03ce9095
zhiailanse <zhiailanse_1@163.com>
2014-03-27 00:18:55 +0800

    commit

---
+++ AndroidAllTest/src/com/zmm/allproject/mainUI/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.zmm.allproject.mainUI;
+
+import com.zmm.allproject.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about_activity);
+	}
+}
